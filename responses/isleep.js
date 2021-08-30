3a1049fe8f0ee732624f8c8e3fc87fb67bcae034
Dysterio <65984254+Dysterio@users.noreply.github.com>
2021-08-30 23:23:11 +0000

    Removed cringe responses😩

--- responses/isleep.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "<:isleep:788228137074425866>",
-  async execute(message) {
-    if (message.guildId !== "442268458072276992") return;
-    if (message.content !== "<:isleep:788228137074425866>") return;
-    const emoji = message.guild.emojis.cache.find(emoji => emoji.name === "nosleep");
-    await message.react(emoji);
-  },
-}
--- responses/monkey.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "monkey",
-    async execute(message) {
-        await message.react("🐵");
-    }
-}
