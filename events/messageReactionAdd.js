20e5bde4c38d2c73cd0bec56057d911066726701
Dysterio <65984254+Dysterio@users.noreply.github.com>
2021-08-30 22:56:04 +0000

    Separated jisleep function

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -5,11 +5,13 @@ module.exports = {
             try { await reaction.fetch(); }
             catch(error) { return console.log(error); }
         }
-        // Error Check
-        if (reaction.message.guildId !== "442268458072276992") return;
+        this.jacobISleep(reaction, user);
+    },
+    jacobISleep: async (reaction, user) => {
+      if (reaction.message.guildId !== "442268458072276992") return;
         if (user.id !== "202206936601460736") return;
         if (reaction.emoji.name !== "isleep") return;
         const emoji = reaction.message.guild.emojis.cache.find(emoji => emoji.name === "nosleep");
         await reaction.message.react(emoji);
-    },
+    }
 };
