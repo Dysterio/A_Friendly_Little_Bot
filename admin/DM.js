9df455231cc8a59c1aa33ae736872341841e5835
Dysterio <65984254+Dysterio@users.noreply.github.com>
2021-08-30 04:55:19 +0000

    Fixed DM args error

--- admin/DM.js
+++ admin/DM.js
@@ -1,17 +1,25 @@
 module.exports = {
     name: "DM",
     async execute(message, args) {
-        if (args.length < 2) return;
+        if (args.length < 1) return;
         const recipientID = args.shift();
         const msg = args.join(" ");
 
         message.client.users.fetch(recipientID).then(recipient => {
-            recipient.send({
-                content: msg,
-                embeds: message.embeds,
-                files: Array.from(message.attachments.values()),
-                components: message.components
-            }).then(() => { message.reply("DM sent successfully!"); });
+            if (msg === "") {
+              recipient.send({
+                  embeds: message.embeds,
+                  files: Array.from(message.attachments.values()),
+                  components: message.components
+              }).then(() => { message.reply("DM sent successfully!"); });
+            } else {
+              recipient.send({
+                  content: msg,
+                  embeds: message.embeds,
+                  files: Array.from(message.attachments.values()),
+                  components: message.components
+              }).then(() => { message.reply("DM sent successfully!"); });
+            }
         });
     }
 }
