bfaf94c8259ce800132698f298c87ccf67beb0e5
Dysterio <65984254+Dysterio@users.noreply.github.com>
2021-08-31 09:55:57 +0000

    Fixed ready event error

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
-        client.users.cache.get(process.env.ADMIN_ID).then(admin => {
+        client.users.fetch(process.env.ADMIN_ID).then(admin => {
             admin.send("Bot is online!");
         });
         client.logger.info(`\n\t\tReady! Logged in as ${client.user.tag}`);
