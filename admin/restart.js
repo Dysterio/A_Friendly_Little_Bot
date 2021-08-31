ad4f3e26a5dc81dfdb8a896d28153b35146e693a
Dysterio <aroradeep@ecs.vuw.ac.nz>
2021-08-31 21:14:47 +1200

    Replace pull code with reset for git

--- admin/restart.js
+++ admin/restart.js
@@ -9,8 +9,8 @@ module.exports = {
         const hardRestart = args[0] === "true";
         let output = "";
         if (hardRestart)
-            output += "```" + (await execSync("git pull", { encoding: "utf-8" })) + "```";
-        output += "```" + (await execSync("busybox reboot", { encoding: "utf-8" })) + "```";
+            output += "```" + (await execSync("git reset --hard origin/master", { encoding: "utf-8" })) + "```";
+        await execSync("busybox reboot", { encoding: "utf-8" });
         message.reply(output);
         message.client.logger.warn(((hardRestart) ? "Hard" : "") + "Restarted the bot");
     }
