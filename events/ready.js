2f86ac9ff7262f96a5dd70527d8427d6c828d6c7
Dysterio <aroradeep@ecs.vuw.ac.nz>
2021-08-31 22:07:22 +1200

    Editted online message

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = {
     once: true,
     async execute(client) {
         client.users.fetch(process.env.ADMIN_ID).then(admin => {
-            admin.send("Bot is online!");
+            admin.send("🟢 Bot is online! 🟢");
         });
         client.logger.info(`\n\t\t\tReady! Logged in as ${client.user.tag}`);
     }
