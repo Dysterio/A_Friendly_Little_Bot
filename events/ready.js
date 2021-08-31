41040684e08bf9cf36197ce5d4098b40ea75fbe6
Dysterio <aroradeep@ecs.vuw.ac.nz>
2021-08-31 21:57:08 +1200

    Restart command check

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,6 @@ module.exports = {
         client.users.fetch(process.env.ADMIN_ID).then(admin => {
             admin.send("Bot is online!");
         });
-        client.logger.info(`\n\t\tReady! Logged in as ${client.user.tag}`);
+        client.logger.info(`\n\t\t\tReady! Logged in as ${client.user.tag}`);
     }
 }
