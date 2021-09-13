be1c471362ed4ce3974c4af8c981b67193dc41b3
Dysterio <aroradeep@ecs.vuw.ac.nz>
2021-09-14 11:33:52 +1200

    Cleaned code

--- responses/!loli.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "!loli",
-    async execute(message) {
-        await message.channel.send("<@202206936601460736>");
-    }
-}
--- startup.sh
+++
@@ -1,4 +0,0 @@
-while true; do
-  npm install node@16
-  npm start
-done
